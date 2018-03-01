02dffec262ff278f8afd231b7add59f735d85ede
Nick Trentacoste <ntrentacoste@v12data.com>
2018-03-01 15:33:04 -0500

    Initial Commit

--- WordWrap/Program.cs
+++ WordWrap/Program.cs
@@ -6,7 +6,13 @@ namespace WordWrap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var wrapper = new WordWrapper();
+
+            var text = "Lorem ipsum dolor sit amet, eum te soleat oporteat accusamus. Ex mutat conceptam pro, vel exerci debitis probatus ad. Delectus lobortis perpetua ius an, et ius porro inermis consetetur, sit et dicant delicata signiferumque. Has atqui posidonium an, te sed odio torquatos, vidisse civibus propriae pri ei.";
+
+            Console.WriteLine(wrapper.Wrap(text, 13));
+
+            Console.ReadLine();
         }
     }
 }
---
+++ WordWrap/WordWrapper.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordWrap
+{
+    public class WordWrapper
+    {
+        public string Wrap(string text, int length)
+        {
+            var wrapped = new StringBuilder();
+            var regex = new Regex("\b");
+            var words = regex.Replace(text, " ").Split(' ');
+            var lineLength = 0;
+
+            foreach (var word in words)
+            {
+                var wordLength = word.Length;
+
+                if (wordLength <= length && (wordLength + lineLength) <= length)
+                {
+                    wrapped.Append(word + " ");
+                    lineLength += (wordLength + 1);
+                }
+                else if (wordLength <= length)
+                {
+                    wrapped.AppendLine();
+                    wrapped.Append(word + " ");
+                    lineLength = (wordLength + 1);
+                }
+                else
+                {
+                    wrapped.AppendLine();
+                    var tempWord = word;
+
+                    do
+                    {
+                        var firstPart = tempWord.Length >= length ? tempWord.Substring(0, length - 1) : tempWord;
+                        if (firstPart.Length < (length - 1))
+                        {
+                            wrapped.Append(firstPart + " ");
+                            lineLength += firstPart.Length;
+                        }
+                        else
+                        {
+                            wrapped.Append(firstPart + "-");
+                            wrapped.AppendLine();
+                            lineLength = 0;
+                        }
+                        
+                        tempWord = tempWord.Replace(firstPart, "");
+                        wordLength -= firstPart.Length;
+                    }
+                    while (wordLength > 0);
+                }
+            }
+
+            return wrapped.ToString();
+        }
+    }
+}
